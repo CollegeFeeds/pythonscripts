29e98afd0c747ab35e118b386d35a0e98a29c70a
harsh <agharsh11@gmail.com>
2017-02-02 21:42:35 +0530

    undergrad updated site scrapper

--- duundergradresults.py
+++ duundergradresults.py
@@ -9,38 +9,32 @@ cursor2.execute(cmd)
 alt=cursor2.fetchone()
 db.commit()
 titlef=alt[1]
-k=[]
-m=[]
-src=requests.get("http://www.du.ac.in/du/index.php?page=students-welfare").text
+src=requests.get("http://exam.du.ac.in/UG-result.html").text
 soup=bs(src,"html.parser")
-soup1=soup.find_all('div',class_="block")[1]          
+soup1=soup.find_all('div',id="Wrapper3")[0]          
 soup2=soup1.find_all('a') 
 file1=open('undergradresults.txt','w+')
 counter=alt[0]
 #print "0"
+v=0;
+m=[]
+k=[]
 for i in soup2:
+     v=v+1;
      link=i["href"]
-     title=i["title"]
-     #print "1"
-     if r==0:
-        r=r+1
-        titleff=title
+     if v<4:
+         continue
+     title="".join([str(j) for j in i.contents])
+     
      if title == alt[1]:
-        break
-     titlef=title
-     #print "2"
-     k.append(title)
+         cursor2.execute("""update undergradcounters set undergradtitle=%s""",(titlef,))
+         break 
      m.append(link)
-cursor2.execute("""update undergradcounters set undergradtitle=%s""",(titleff,))
+     k.append(title)
+     titlef=title
 while len(m) !=0:
-     src2=requests.get(m.pop()).text
-     soup3=bs(src2,"html.parser")
-     soup4=soup3.find_all("div",class_="content-inner grid_12")[1].find_all("a")[0]
-     linkf=soup4["href"]
-     sql="""insert into undergradresults(id,title,linkf) values(NULL,'%s','%s')"""%(k.pop(),linkf)
+     sql="""insert into undergradresults(id,title,linkf) values(NULL,'%s','%s')"""%(k.pop(),m.pop())
      cursor2.execute(sql)
-     file1.write('\nhttp://www.du.ac.in/du/%s'%linkf)
-     #print "3"
      counter=counter+1
      
 file1.close()
--- starttable.py
+++ starttable.py
@@ -33,41 +33,41 @@ cmd="""drop table if exists srccnotices"""
 cursor.execute(cmd)
 cmd="""drop table if exists hansrajnotices"""
 cursor.execute(cmd)
-cmd="""create table undergradresults(id int primary key auto_increment,title char(375) not null,linkf char(375) not null)"""
+cmd="""create table undergradresults(id int primary key auto_increment,title char(255) not null,linkf char(255) not null)"""
 cursor.execute(cmd)
-cmd="""create table postgradresults(id int primary key auto_increment,title char(375) not null,linkf char(375) not null)"""
+cmd="""create table postgradresults(id int primary key auto_increment,title char(255) not null,linkf char(255) not null)"""
 cursor.execute(cmd)
-cmd="""create table postgradcounters(postgradid int,postgradtitle char(375))"""
+cmd="""create table postgradcounters(postgradid int,postgradtitle char(255))"""
 cursor.execute(cmd)
-cmd="""create table undergradcounters(undergradid int,undergradtitle char(375))"""
+cmd="""create table undergradcounters(undergradid int,undergradtitle char(255))"""
 cursor.execute(cmd)
 cmd="""insert into undergradcounters(undergradid,undergradtitle) values(1,"last")"""
 cursor.execute(cmd)
 cmd="""insert into postgradcounters(postgradid,postgradtitle) values(1,"last")"""
 cursor.execute(cmd)
-cmd="""create table mphilresults(id int primary key auto_increment,title char(375) not null,linkf char(375) not null)"""
+cmd="""create table mphilresults(id int primary key auto_increment,title char(255) not null,linkf char(255) not null)"""
 cursor.execute(cmd)
-cmd="""create table mphilcounters(mphilid int,mphiltitle char(375))"""
+cmd="""create table mphilcounters(mphilid int,mphiltitle char(255))"""
 cursor.execute(cmd)
-cmd="""create table phdresults(id int primary key auto_increment,title char(375) not null,linkf char(375) not null)"""
+cmd="""create table phdresults(id int primary key auto_increment,title char(255) not null,linkf char(255) not null)"""
 cursor.execute(cmd)
-cmd="""create table phdcounters(phdid int,phdtitle char(375))"""
+cmd="""create table phdcounters(phdid int,phdtitle char(255))"""
 cursor.execute(cmd)
-cmd="""create table diplomaresults(id int primary key auto_increment,title char(375) not null,linkf char(375) not null)"""
+cmd="""create table diplomaresults(id int primary key auto_increment,title char(255) not null,linkf char(255) not null)"""
 cursor.execute(cmd)
-cmd="""create table diplomacounters(diplomaid int,diplomatitle char(375))"""
+cmd="""create table diplomacounters(diplomaid int,diplomatitle char(255))"""
 cursor.execute(cmd)
-cmd="""create table ncwebresults(id int primary key auto_increment,title char(375) not null,linkf char(375) not null)"""
+cmd="""create table ncwebresults(id int primary key auto_increment,title char(255) not null,linkf char(255) not null)"""
 cursor.execute(cmd)
-cmd="""create table ncwebcounters(ncwebid int,ncwebtitle char(375))"""
+cmd="""create table ncwebcounters(ncwebid int,ncwebtitle char(255))"""
 cursor.execute(cmd)
-cmd="""create table srccnotices(id int primary key auto_increment,title char(375) not null,linkf char(375) not null)"""
+cmd="""create table srccnotices(id int primary key auto_increment,title char(255) not null,linkf char(255) not null)"""
 cursor.execute(cmd)
-cmd="""create table srcccounters(srccid int,srcctitle char(375))"""
+cmd="""create table srcccounters(srccid int,srcctitle char(255))"""
 cursor.execute(cmd)
-cmd="""create table hansrajnotices(id int primary key auto_increment,title char(375) not null,linkf char(375) not null)"""
+cmd="""create table hansrajnotices(id int primary key auto_increment,title char(255) not null,linkf char(255) not null)"""
 cursor.execute(cmd)
-cmd="""create table hansrajcounters(hansrajid int,hansrajtitle char(375))"""
+cmd="""create table hansrajcounters(hansrajid int,hansrajtitle char(255))"""
 cursor.execute(cmd)
 db.commit()
 cursor.close()
