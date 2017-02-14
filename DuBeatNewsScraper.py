8930a6113b7441c49a0158fd13f384e56190d5c4
icancode23 <ernipunarora@gmail.com>
2017-02-15 00:02:39 +0530

    complete du beat headlines scraper

--- DuBeatNewsScraper.py
+++ DuBeatNewsScraper.py
@@ -8,6 +8,12 @@ db.set_character_set('utf8')
 cursor.execute('SET NAMES utf8;')
 cursor.execute('SET CHARACTER SET utf8;')
 cursor.execute('SET character_set_connection=utf8;')
+previous_headlines=[]
+cmd="""select * from api_headlines"""
+cursor.execute(cmd)
+previous_data=cursor.fetchall()
+for headline in previous_data:
+	previous_headlines.append(headline[1])
 sql_query="""DELETE FROM api_headlines WHERE 1=1"""
 cursor.execute(sql_query)
 db.commit()
@@ -23,13 +29,14 @@ soup=bs(src,"html.parser")
 soup1=soup.find_all('ul',class_="feature-post-list")[0]
 soup2=soup1.find_all('a',class_="feature-image-link")
 headline_links=[]
+currentheadline_titles=[]
 image_links=[]
 ###################### soup2 contains all the anchor tags that contain the main headline"###################
 for headline in soup2:
 	# print "The title for the headline is: ",headline['title']
 	# print "The link is ",headline['href']
 	try:
-		print headline['title']
+		currentheadline_titles.append(headline['title'])
 		headline_links.append(headline['href'].encode('utf-8'))
 		sql_query="""INSERT INTO api_headlines(id,title,linkf,imagelink) VALUES(NULL,'%s','%s','null')"""%(headline['title'].encode('utf-8'),headline['href'].encode('utf-8'))
 		#print sql_query
@@ -37,23 +44,24 @@ for headline in soup2:
 
 	except Exception as e:
 		print "Database Exception :",e
-i=0
+
 for link in headline_links:
 	a=requests.get(link)
 	soupa=bs(a.text,"html.parser")
 	imgsrc=soupa.find_all('div',class_="single_post_format_image")[0].find_all('img')[0]['src'].encode('utf-8')
 	image_links.append(imgsrc)
 	sql_query="""UPDATE api_headlines SET imagelink='%s' WHERE linkf='%s' """%(imgsrc,link)
-	print sql_query
 	cursor.execute(sql_query)
 	db.commit()
-	i=i+1
-	print i
-
-print image_links
-
-
-
+######################### DEBUG PRINTS #######################
+print "The previous headlines are",previous_headlines
+print "The current headlines are",currentheadline_titles
+ ####################### Check Whether to update banners and then publish the status#######
+ if set(previous_headlines).issubset(set(currentheadline_titles)):
+ 	if set(previous_headlines)==set(currentheadline_titles):
+ 		pass
+ 	else:
+ 		########## Notify all app users that there are some changes #######################
 db.commit()
 db.close()
 
