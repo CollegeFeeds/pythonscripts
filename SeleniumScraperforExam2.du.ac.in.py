f7fe27633a04f40fc340c4d6abebdc484235629b
icancode23 <ernipunarora@gmail.com>
2017-03-13 13:59:31 +0530

    Added virtualDisplay Support

--- SeleniumScraperforExam2.du.ac.in.py
+++ SeleniumScraperforExam2.du.ac.in.py
@@ -1,12 +1,17 @@
+from pyvirtualdisplay import Display
 from selenium import webdriver as wb
 import time
-pathtochromedriver = '/Users/nipunarora/Desktop/django/webscraper&djangostart/seleniumscraper/chromedriver'
+#pathtochromedriver = '/Users/nipunarora/Desktop/django/webscraper&djangostart/seleniumscraper/chromedriver'### path to chromedriver Nipuns laptop
+pathtochromedriver='/usr/local/share/chromedriver' ##ubuntu chromedriver path
+################## A Virtual Display for VPS #########################
+display = Display(visible=0, size=(800, 600))
+display.start()
 driver = wb.Chrome(executable_path=pathtochromedriver)  
 driver.get('http://duexam2.du.ac.in/RSLT_ND2016/Students/List_Of_Declared_Results.aspx')
 a=driver.find_element_by_id("gvshow_ata_glance_ctl05_btn_show_details")
 a.click()
 time.sleep(20)
-current_page_source=open("Pagesource.html","a+")
+current_page_source=open("Pagesource1.html","a+")
 current_page_source.write(driver.page_source.encode('utf-8'))
 current_page_source.close()
 print "Completed Successfully"
