da802c2944158b7ec6a16c88dc11fe12bdeea58e
icancode23 <ernipunarora@gmail.com>
2017-03-13 14:04:36 +0530

    Added virtualDisplay Support

--- SeleniumScraperforExam2.du.ac.in.py
+++ SeleniumScraperforExam2.du.ac.in.py
@@ -7,9 +7,11 @@ pathtochromedriver='/usr/local/share/chromedriver' ##ubuntu chromedriver path
 display = Display(visible=0, size=(800, 600))
 display.start()
 driver = wb.Chrome(executable_path=pathtochromedriver)  
+print "driver executed"
 driver.get('http://duexam2.du.ac.in/RSLT_ND2016/Students/List_Of_Declared_Results.aspx')
 a=driver.find_element_by_id("gvshow_ata_glance_ctl05_btn_show_details")
 a.click()
+print "Clicked"
 time.sleep(20)
 current_page_source=open("Pagesource1.html","a+")
 current_page_source.write(driver.page_source.encode('utf-8'))
